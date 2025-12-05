7979db5312e20cb75760575d4634497423f82664
victor <motzvek@gmail.com>
2025-12-05 19:57:24 -0400

    Add async to launch pipeline function

--- src/main/java/com/biopatternsg/infrastructure/adapters/in/restcontrollers/ExperimentController.java
+++ src/main/java/com/biopatternsg/infrastructure/adapters/in/restcontrollers/ExperimentController.java
@@ -5,9 +5,13 @@ import com.biopatternsg.domain.port.in.LaunchPipeline;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 @Slf4j
 @ApplicationScoped
@@ -16,12 +20,23 @@ import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 public class ExperimentController {
 
     private final LaunchPipeline launchExperiment;
+    private final Executor executor;
 
     @POST
-    @Path("/launch-experiment")
-    public void experiment(@RequestBody PipelineConfig launchExperimentRequest){
+    @Path("/launch-pipeline")
+    public Response experiment(@RequestBody PipelineConfig launchExperimentRequest) {
 
-        launchExperiment.execute(launchExperimentRequest);
+        CompletableFuture.runAsync(() -> {
+            try {
+                launchExperiment.execute(launchExperimentRequest);
+            } catch (Exception e) {
+                log.error("Error launch pipeline", e);
+            }
+        }, executor);
+
+        return Response.accepted()
+                .entity("{\"message\": \"successful launch pipeline " + launchExperimentRequest.getPipelineId() + "\"}")
+                .build();
     }
 
 }
