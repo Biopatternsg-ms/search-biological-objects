29aee225e155bdee9adac69b6fa492594111e608
Yorman <yorman.cas@gmail.com>
2026-06-30 22:06:36 -0400

    fix test typo

--- src/test/java/archtest/ArchitectureLayerTest.java
+++ src/test/java/archtest/ArchitectureLayerTest.java
@@ -91,7 +91,7 @@ class ArchitectureLayerTest {
     @Test
     void controllersNamingConventionTest() {
         ArchRuleDefinition.classes()
-                .that().resideInAPackage("..infrastructure.adaptars.in.restcontrollers..")
+                .that().resideInAPackage("..infrastructure.adapters.in.restcontrollers..")
                 .should().haveSimpleNameEndingWith("Controller")
                 .check(this.javaClasses);
     }
@@ -100,7 +100,7 @@ class ArchitectureLayerTest {
     @Test
     void adaptersNamingConventionTest() {
         ArchRuleDefinition.classes()
-                .that().resideInAPackage("..infrastructure.adaptars.out..")
+                .that().resideInAPackage("..infrastructure.adeptars.out..")
                 .should().haveSimpleNameEndingWith("Adapter")
                 .check(this.javaClasses);
     }
